0c3baaf247354155d332b461fbf421334fbc38eb
S S ROHAN <singadasarirohan24@gmail.com>
2026-04-13 18:01:45 +0530

    final UI + fixes + role + deployment ready

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,11 @@ connectDB();
 const app = express();
 
 
-app.use(cors());
+
+app.use(cors({
+  origin: "https://task-tracker-brown-ten.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 // app.use(cors());
 
